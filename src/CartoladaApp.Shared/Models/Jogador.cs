a8ea763c62bb92cda92ae6c8f988279db6ef45c6
Juracy Junior <juracy.junior@prestadores.amil.com.br>
2017-06-29 15:44:34 -0300

    Commit of Project

---
+++ src/CartoladaApp.Shared/Models/Jogador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CartoladaApp.Shared.Models
+{
+    public class Jogador
+    {
+        [JsonProperty("atleta_id")]
+        public int Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Apelido { get; set; }
+
+        [JsonProperty("foto")]
+        public String UrlFoto { get; set; }
+    }
+}
---
+++ src/CartoladaApp.Shared/Models/MercadoDestaque.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CartoladaApp.Shared.Models
+{
+    public class MercadoDestaque : Jogador
+    {
+        public int Escalacoes { get; set; }
+
+        public String Clube { get; set; }
+
+        [JsonProperty("escudo_clube")]
+        public String UrlEscudoClube { get; set; }
+    }
+}
